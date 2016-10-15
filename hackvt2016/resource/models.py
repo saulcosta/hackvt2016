c862380229511231dc6b92bd7141bddc0eb1c505
Saul Costa <saulcosta18@gmail.com>
2016-10-14 22:33:34 -0400

    Added long / lat columns to resources.

--- hackvt2016/resource/models.py
+++ hackvt2016/resource/models.py
@@ -13,6 +13,8 @@ class Resource(SurrogatePK, Model):
     description = Column(db.String, nullable=False)
     host = Column(db.String, nullable=True)
     email = Column(db.String, nullable=True)
+    longitude = Column(db.Float, nullable=True)
+    latitude = Column(db.Float, nullable=True)
 
     category_id = reference_col('categories', nullable=False)
 
