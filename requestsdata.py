5b22c63ec20c3e5204431c4a797999361eccfb33
Saul Costa <saulcosta18@gmail.com>
2016-10-15 05:24:36 -0400

    Started on seed data.

--- requestsdata.py
+++ requestsdata.py
@@ -5,10 +5,31 @@ from hackvt2016.category.models import Category
 
 def main():
     create_app().app_context().push()
+    Resource.query.delete()
+    load_libraries()
+    load_seeds()
+
+
+
+def load_seeds():
+    resources = [
+        ('Sports', 'Soccer Game', 'Everyone is invited to a quick soccer game this weekend.', 'Saul Costa', 'saulcosta18222@gmail.com')
+    ]
+    for (category, title, description, host, email, longitude, latitude) in resources:
+        categoryID = Category.query.filter_by(name=category).first().id
+        Resource.create(
+            category_id=categoryID,
+            title=title,
+            description=description,
+            host=host,
+            email=email,
+            longitude=longitude,
+            latitude=latitude)
+
+
+def load_libraries():
     thisRequest = requests.get('https://data.vermont.gov/resource/g5rt-gwwe')
     jsonObject = thisRequest.json()
-
-    Resource.query.delete()
 
     for entry in jsonObject:
         category = 'library'
---
+++ seed_resources.py
@@ -0,0 +1,24 @@
+'''
+Load's seed category data.
+'''
+from hackvt2016.app import create_app
+from hackvt2016.category.models import Category
+
+
+def main():
+    create_app().app_context().push()
+    Category.query.delete()
+    categories = [
+        ('Library', 'book'),
+        ('Event', 'map-pin'),
+        ('Sports', 'soccer-ball-o'),
+        ('Cool Stuff', 'search'),
+        ('Museum', 'bookmark'),
+        ('Resource', 'pencil'),
+        ('Nature Site', 'pagelines')]
+    for (name, icon) in categories:
+        Category.create(name=name, icon=icon)
+
+
+if __name__ == '__main__':
+    main()
