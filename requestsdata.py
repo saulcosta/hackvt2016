4385eb164b4466afb2ef725879fb84b5661935d7
Nyrene <realrachael@gmail.com>
2016-10-15 11:59:21 -0400

    added more resources

--- requestsdata.py
+++ requestsdata.py
@@ -39,13 +39,41 @@ long: (-71.739) - (-72.632)
             ('Museum', 'Middle Age Weapons Musuem', 'Open 10-5 Daily', 'Brianna Wright', 'mawm@museums.org'),
             ('Nature Site', 'Hunter Trail', 'Requires appropriate footwear', '', ''),
             ('Cool Stuff', 'Alden Partridge Monument', 'In memory of the Norwich University Founder', '', ''),
-            ('Cool Stuff', 'Ski Range', 'Bring your skis!', '', ''),
+            ('Sports', 'Ski Range', 'Bring your skis!', '', ''),
             ('Sports', 'Karate Lessons', 'Tae Kwon Do', 'Sensei Vivian', 'themaster@thekaratestudio.com'),
             ('Nature Site', 'Crystal Mine Lake', 'No lifeguard on duty', 'Trevor Daniels', 'fri@.mns.org'),
             ('Resource', 'School Supplies and Book Store', 'For all your education needs!', '', 'theotherstaples@bookstores.com'),
             ('Musuem', 'Stone House Historical Center', 'With live in actors', 'Manny Curtis', 'mcurtis@stonehouse.org'),
             ('Musuem', 'VT Historical Archives', 'Open 8 to 4 on weekdays', '', 'admin@vtarchives.org'),
             ('Resource', 'Musical Studio', 'Instruments and soundrooms available to reserve', '', 'mstudio@vtmusic.net')
+
+            ('Museum', 'Black Manor Historical House', 'Provides historical reenactments on Mondays and Thursdays', 'Tia Ramirez', 'tramirez@blackmanor.edu')
+
+            ('Nature Site', 'Red Fern Bike Trail', 'Maps are available at all entrances.', '', 'infotrails@parksandrecvt.gov')
+            ('Nature Site', 'Westerman Bird Viewing Platform', '', '', 'infobirds@parksandrecvt.gov')
+            ('Nature Site', 'Holmes Stargazing Platform', 'Parking is available down the road.', '', 'jweng@vtastronomy.org')
+
+            ('Event', 'Ski Race', 'Open to grades 1-5', 'Scoville J Danis"' 'scjda12@yahoos.org')
+            ('Event', 'Guitar Lessons', 'Specialty: acoustic', 'Wren Fido', 'fido@onestopguitar.com')
+            ('Event', 'Stargazing', 'Bring a coat and blanket for the Fall stargazing event!', 'Julian Weng', 'jweng@vtastronomy.org')
+            ('Event', 'Band Tryouts', 'Instrument rentals can be arranged beforehand with the contact person.' 'Valerie Collins', 'vcollins@themusicstudio.org')
+
+
+            ('Cool Stuff', 'Sundial', 'Built in 1834', '', '')
+            ('Cool Stuff', 'Geodome', '', '', '')
+            ('Cool Stuff', 'Historical Cemetery', 'Established in 1782', '', '')
+            ('Cool Stuff', 'Old Weeping Willow Tree', 'Planted 1900', '', '')
+
+            ('Sports', 'Horseback Riding Lessons', 'Beginner to Advanced lessons provided!', 'Leonard McGarth', 'horseback@horsebackvt.info')
+            ('Sports', 'Hockey Tryouts', 'Open to grades 5-8, gear provided', 'Olivia Olsen', 'olsen@watervillemiddle.com')
+            ('Sports', 'Swimming Lessons', 'Group classes and one-on-one mentoring offered', '', 'lessons@swimvt.com')
+            ('Sports', 'Cross Country Practice', 'We will be starting with 3 miles on Tuesday.', 'Gina Woo', 'gwoo@tritonhs.com')
+
+
+
+
+
+
         ]
         for (category, title, description, host, email) in resources:
             category = Category.query.filter_by(name=category).first()
