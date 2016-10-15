adc719e31328f604e22952c0472b237158f74be7
Saul Costa <saulcosta18@gmail.com>
2016-10-15 04:48:16 -0400

    Merged templates and handle data.

--- requestsdata.py
+++ requestsdata.py
@@ -7,24 +7,19 @@ def main():
     create_app().app_context().push()
     thisRequest = requests.get("https://data.vermont.gov/resource/g5rt-gwwe")
     jsonObject = thisRequest.json()
-    jsonObject[0]
 
-    #Resource.query.delete()
-    #Category.create(id=1, name="library", )
-
+    Resource.query.delete()
 
     for entry in jsonObject:
         category = "library"
-        latitude = entry.get("latitude")
-        longitude = entry.get("longitude")
-        location = "latitude" + "," + "longitude"
+        latitude = entry.get('location_1').get("latitude")
+        longitude = entry.get('location_1').get("longitude")
         title = entry.get("library")
         description = "website: " + str(entry.get("web_location"))
         categoryID = Category.query.filter_by(name='Library').first().id
 
-
-        #Resource.create(title=title, description=description,host=host, email=email, category_id=categoryID)
-        Resource.create(title=title, description=description, category_id=categoryID,)
+        if latitude and longitude:
+            Resource.create(title=title, description=description, category_id=categoryID, latitude=latitude, longitude=longitude)
 
 
 if __name__ == '__main__':
