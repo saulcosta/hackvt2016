dc61c995492a88e3a8b539b183dca8073502b7c6
Saul Costa <saulcosta18@gmail.com>
2016-10-15 04:52:25 -0400

    Linting.

--- requestsdata.py
+++ requestsdata.py
@@ -5,17 +5,17 @@ from hackvt2016.category.models import Category
 
 def main():
     create_app().app_context().push()
-    thisRequest = requests.get("https://data.vermont.gov/resource/g5rt-gwwe")
+    thisRequest = requests.get('https://data.vermont.gov/resource/g5rt-gwwe')
     jsonObject = thisRequest.json()
 
     Resource.query.delete()
 
     for entry in jsonObject:
-        category = "library"
-        latitude = entry.get('location_1').get("latitude")
-        longitude = entry.get('location_1').get("longitude")
-        title = entry.get("library")
-        description = "website: " + str(entry.get("web_location"))
+        category = 'library'
+        latitude = entry.get('location_1').get('latitude')
+        longitude = entry.get('location_1').get('longitude')
+        title = entry.get('library')
+        description = 'website: ' + str(entry.get('web_location'))
         categoryID = Category.query.filter_by(name='Library').first().id
 
         if latitude and longitude:
